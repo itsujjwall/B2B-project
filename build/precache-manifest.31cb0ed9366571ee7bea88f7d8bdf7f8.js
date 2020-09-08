685298e8ddc31ae071b5f1c18ba1c028d7a1e7fa
Ronaksengupta99 <55063393+r1999-ron@users.noreply.github.com>
2020-09-08 22:33:43 +0530

    Add files via upload

---
+++ build/precache-manifest.31cb0ed9366571ee7bea88f7d8bdf7f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f087e2bb9b8360c7284cb8c82980bee",
+    "url": "/1704546/index.html"
+  },
+  {
+    "revision": "f7090e672533969c4f93",
+    "url": "/1704546/static/css/main.ccc37d87.chunk.css"
+  },
+  {
+    "revision": "7c6166711ec0d7a91063",
+    "url": "/1704546/static/js/2.d8ba3cfb.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "/1704546/static/js/2.d8ba3cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7090e672533969c4f93",
+    "url": "/1704546/static/js/main.e74006da.chunk.js"
+  },
+  {
+    "revision": "fde59a769bdd08b68879",
+    "url": "/1704546/static/js/runtime-main.73c81556.js"
+  },
+  {
+    "revision": "bcd3becb85f1804b6345b95011fccac5",
+    "url": "/1704546/static/media/attach_money.bcd3becb.svg"
+  },
+  {
+    "revision": "6b91d014985480b0690166ccb0d10dab",
+    "url": "/1704546/static/media/avatar.6b91d014.svg"
+  },
+  {
+    "revision": "593dca1a4b65dad1e5b8ea614b57478d",
+    "url": "/1704546/static/media/companyLogo.593dca1a.svg"
+  },
+  {
+    "revision": "62543aad6e41eccd5bd828409218f6e0",
+    "url": "/1704546/static/media/john.62543aad.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/1704546/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "77a51d72a5fd8d6e2e3134b6f026023f",
+    "url": "/1704546/static/media/send-24px.77a51d72.svg"
+  }
+]);
